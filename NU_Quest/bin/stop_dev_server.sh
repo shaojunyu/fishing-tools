3278a1e2e709e8b1e49814b25ecd29cda2f56395
Shaojun Yu <shaojun.yu@northwestern.edu>
2024-07-31 04:36:50 -0500

    Update dev container definition and start/stop scripts

--- NU_Quest/bin/stop_dev_server.sh
+++ NU_Quest/bin/stop_dev_server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# check hostname, only  allow quser32
+if [ "$(hostname)" != "quser32" ]; then
+    echo "This script must be run on quser32"
+    exit 1
+fi
 
 module load singularity
 singularity instance stop quest_dev
